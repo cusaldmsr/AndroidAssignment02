ce344166bb909c33429a55a798e94e673347765b
Kusal Damsara <kusal2002d@Gmail.com>
2025-03-28 13:25:58 +0530

    Add icon images, fonts, vectors

--- app/src/main/java/com/cusaldev/androidassignment02/SplashActivity.kt
+++ app/src/main/java/com/cusaldev/androidassignment02/SplashActivity.kt
@@ -1,12 +1,17 @@
 package com.cusaldev.androidassignment02
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SplashActivity : AppCompatActivity() {
+
+    lateinit var imgBtn : ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,13 @@ class SplashActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        imgBtn = findViewById(R.id.imageButton)
+
+        imgBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
